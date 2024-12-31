4f4a7003bb60eeecba231fa8f8720b7e5af04943
AkshAI Arelli <akshayarelli2030@gmail.com>
2024-12-31 13:19:42 +0530

    removed Cart Array index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.use(cors());
 app.use(express.json());
 
 let cart = [
-  { productId: 1, name: 'Laptop', price: 50000, quantity: 1 },
-  { productId: 2, name: 'Mobile', price: 20000, quantity: 2 },
 ];
 
 function addToCart(cart, productId, name, price, quantity) {
